b1b1c32be6bb94bd0d047712a20ca60c216ced6a
P Maryon <maryonpawel@gmail.com>
2021-12-06 11:17:23 +0000

    albums controller cleaned up

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,28 +1,13 @@
 class AlbumsController < ApplicationController
-  require 'json'
-  require 'open-uri'
-  
   def index
-    # @albums = HTTParty.get('https://jsonplaceholder.typicode.com/albums')
-    # @photos = HTTParty.get('https://jsonplaceholder.typicode.com/photos?albumId=1')
-    url_album = 'https://jsonplaceholder.typicode.com/albums'
-    url_photo = 'https://jsonplaceholder.typicode.com/photos?albumId=1'
-    url_users = 'https://jsonplaceholder.typicode.com/users'
-    album_string = open(url_album).read
-    photo_string = open(url_photo).read
-    users = open(url_users).read
-
-    @photos = JSON.parse(photo_string)
-    @albums = JSON.parse(album_string)
-    @users = JSON.parse(users)
     @photos = HTTParty.get("https://jsonplaceholder.typicode.com/photos#{params[:albumId]}")
-
+    @users = HTTParty.get("https://jsonplaceholder.typicode.com/users")
+    @albums = HTTParty.get("https://jsonplaceholder.typicode.com/albums")
   end
 
   def show
     @album = HTTParty.get("https://jsonplaceholder.typicode.com/albums/#{params[:id]}")
     @photos = HTTParty.get("https://jsonplaceholder.typicode.com/photos#{params[:albumId]}")
     @user = HTTParty.get("https://jsonplaceholder.typicode.com/users/#{params[:id]}")
-
   end
 end
