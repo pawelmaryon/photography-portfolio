9c1e008843d56760060383f12b29db30aa0fbf87
P Maryon <maryonpawel@gmail.com>
2021-12-06 10:06:38 +0000

    users page: data done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,7 @@ class UsersController < ApplicationController
   
   def show
    @user = HTTParty.get("https://jsonplaceholder.typicode.com/users/#{params[:id]}")
+   @album = HTTParty.get("https://jsonplaceholder.typicode.com/albums/#{params[:userId]}")
+
   end
 end
