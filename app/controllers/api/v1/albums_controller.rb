edbe324cdd426fd0ee390dcdf26a9451008be47e
P Maryon <maryonpawel@gmail.com>
2021-12-11 12:09:48 +0000

    basic api fetch set

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,10 +1,14 @@
-class AlbumsController < ApplicationController
-  def index
-    @photos = HTTParty.get("https://jsonplaceholder.typicode.com/photos#{params[:albumId]}")
-    @users = HTTParty.get("https://jsonplaceholder.typicode.com/users/#{params[:id]}")
-    @albums = HTTParty.get("https://jsonplaceholder.typicode.com/albums")
-  end
+class Api::V1::AlbumsController < Api::V1::BaseController
 
+  def index
+    # @photos = HTTParty.get("https://jsonplaceholder.typicode.com/photos#{params[:albumId]}")
+    # @users = HTTParty.get("https://jsonplaceholder.typicode.com/users/#{params[:id]}")
+    @albums = HTTParty.get("https://jsonplaceholder.typicode.com/albums")
+
+    render json: @albums
+    
+
+  end
   def show
     @albums = HTTParty.get("https://jsonplaceholder.typicode.com/albums")
     @album = HTTParty.get("https://jsonplaceholder.typicode.com/albums/#{params[:id]}")
